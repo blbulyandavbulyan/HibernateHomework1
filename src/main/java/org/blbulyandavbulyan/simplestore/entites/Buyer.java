6a221835fe436053c315edeaaae8e07628ce9c53
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-05 19:44:14 +0400

    Buyer entity added

---
+++ src/main/java/org/blbulyandavbulyan/simplestore/entites/Buyer.java
@@ -0,0 +1,31 @@
+package org.blbulyandavbulyan.simplestore.entites;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Данный класс предоставляет сущность покупателя, у которого есть ИД и имя
+ */
+@Entity
+@Table(name = "buyers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Buyer {
+    /**
+     * ИД покупателя
+     */
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    /**
+     * Имя покупателя
+     */
+    @Column(name = "name")
+    private String name;
+}
