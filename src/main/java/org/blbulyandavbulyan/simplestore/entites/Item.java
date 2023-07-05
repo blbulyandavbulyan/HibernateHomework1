1692138efe00321fe0c92166b88a28493caf26aa
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-05 19:44:24 +0400

    Item entity added

---
+++ src/main/java/org/blbulyandavbulyan/simplestore/entites/Item.java
@@ -0,0 +1,36 @@
+package org.blbulyandavbulyan.simplestore.entites;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Данный класс предоставляет сущность для товара у которого есть ИД, название и текущая цена
+ */
+@Entity
+@Table(name = "items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+    /**
+     * ИД товара в базе
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    /**
+     * Название товара
+     */
+    @Column(name = "title")
+    private String title;
+    /**
+     * Текущая цена
+     */
+    @Column(name = "price")
+    private Long price;
+}
