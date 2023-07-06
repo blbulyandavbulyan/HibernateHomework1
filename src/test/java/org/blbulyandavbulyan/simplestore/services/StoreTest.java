eb8832d2b601ab3f2ff23413c953abca0f097563
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-06 14:16:17 +0400

    StoreTest for testing class Store was added

---
+++ src/test/java/org/blbulyandavbulyan/simplestore/services/StoreTest.java
@@ -0,0 +1,26 @@
+package org.blbulyandavbulyan.simplestore.services;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.blbulyandavbulyan.simplestore.utils.ORMUtils;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.blbulyandavbulyan.simplestore.utils.SetupDatabase.createTables;
+import static org.blbulyandavbulyan.simplestore.utils.SetupDatabase.dropTables;
+
+public class StoreTest extends IStoreTest{
+    private final static EntityManagerFactory emf = ORMUtils.createEntityManagerFactory();
+    @Override
+    @BeforeEach
+    void setUp() {
+        try {
+            dropTables(emf);
+            createTables(emf);
+            store = new Store(emf);
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
