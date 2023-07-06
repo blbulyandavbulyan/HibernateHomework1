f71471fa3888517b011d59866de66253f704bc43
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-06 14:15:28 +0400

    Added column price in bought_products

--- src/main/resources/sql/setup.sql
+++ src/main/resources/sql/setup.sql
@@ -1,6 +1,7 @@
+
 CREATE TABLE IF NOT EXISTS products(product_id bigserial PRIMARY KEY, title varchar(255) NOT NULL, price INTEGER NOT NULL);
 CREATE TABLE IF NOT EXISTS consumers(consumer_id bigserial PRIMARY KEY, name VARCHAR(255) NOT NULL);
-CREATE TABLE IF NOT EXISTS bought_products(id bigserial PRIMARY KEY, consumer_id INTEGER NOT NULL, product_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS bought_products(id bigserial PRIMARY KEY, consumer_id INTEGER NOT NULL, product_id INTEGER NOT NULL, price INTEGER NOT NULL,
     FOREIGN KEY(consumer_id) REFERENCES consumers(consumer_id),
     FOREIGN KEY(product_id) REFERENCES products(product_id)
 );
