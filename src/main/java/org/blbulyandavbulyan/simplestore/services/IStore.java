90fc944a00eb200848c78a7d3120e084b808602d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-06 21:06:33 +0400

    Contract for all methods improved in IStore

--- src/main/java/org/blbulyandavbulyan/simplestore/services/IStore.java
+++ src/main/java/org/blbulyandavbulyan/simplestore/services/IStore.java
@@ -15,6 +15,7 @@ public interface IStore {
      * @param name имя покупателя
      * @return коллекцию купленных продуктов или пустую коллекцию, если таковых нет
      * @throws org.blbulyandavbulyan.simplestore.services.exceptions.ConsumerNotFoundException если нет покупателя с таким именем
+     * @throws IllegalArgumentException если name null
      */
     Collection<BoughtProduct> getBoughtProductsByConsumerName(String name);
 
@@ -23,6 +24,7 @@ public interface IStore {
      * @param title имя продукта
      * @return коллекция покупателей или пустая коллекция, если товар никто не купил
      * @throws org.blbulyandavbulyan.simplestore.services.exceptions.ProductNotFoundException если нет товара с таким названием
+     * @throws IllegalArgumentException если title null
      */
     Collection<Consumer> getConsumersByProductTitle(String title);
 
@@ -30,6 +32,7 @@ public interface IStore {
      * Удаляет покупателя
      * @param name имя покупателя, которого нужно удалить
      * @return true если такой покупатель был, иначе false
+     * @throws IllegalArgumentException если name null
      */
     boolean deleteConsumer(String name);
 
@@ -37,6 +40,7 @@ public interface IStore {
      * Удаляет продукт по названию
      * @param title название продукта
      * @return true если такой продукт был, иначе false
+     * @throws IllegalArgumentException если title null
      */
     boolean deleteProduct(String title);
 
@@ -54,12 +58,14 @@ public interface IStore {
     /**
      * Добавляет продукт в магазин
      * @param product продукт, который нужно добавить
+     * @throws IllegalArgumentException если product null
      */
     void addProduct(Product product);
 
     /**
      * Добавляет покупателя в магазин
      * @param consumer покупатель, которого нужно добавить
+     * @throws IllegalArgumentException если consumer null
      */
     void addConsumer(Consumer consumer);
 }
