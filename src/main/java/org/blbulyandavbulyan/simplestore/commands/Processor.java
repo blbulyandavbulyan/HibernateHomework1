d37a27b9561f1ddcf586948c46c560c2bbb2aee3
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-06 17:24:53 +0400

    Added command processor

---
+++ src/main/java/org/blbulyandavbulyan/simplestore/commands/Processor.java
@@ -0,0 +1,68 @@
+package org.blbulyandavbulyan.simplestore.commands;
+
+import org.blbulyandavbulyan.simplestore.entites.Product;
+import org.blbulyandavbulyan.simplestore.services.IStore;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.function.Supplier;
+
+/**
+ * Данный класс предоставляет обработчик текстовых команд для IStore
+ */
+public class Processor {
+
+    private final IStore iStore;
+    private final PrintStream ps;
+
+    public Processor(IStore iStore, PrintStream ps) {
+        this.iStore = iStore;
+        this.ps = ps;
+    }
+
+    public void processCommand(String commandForParsing) {
+        String[] splitCommand = Arrays.stream(commandForParsing.split(" ")).filter(s -> !s.isBlank()).toArray(String[]::new);
+        String action = splitCommand[0];
+        switch (action) {
+            case "/showProductsByConsumer" -> {
+                throwIfInvalidLength(splitCommand, 2);
+                var boughtProducts = iStore.getBoughtProductsByConsumerName(splitCommand[1]);
+                ps.println(boughtProducts);
+            }
+            case "/showConsumersByProductTitle" -> {
+                throwIfInvalidLength(splitCommand, 2);
+                var consumers = iStore.getConsumersByProductTitle(splitCommand[1]);
+                ps.println(consumers);
+            }
+            case "/deleteConsumer" -> {
+                throwIfInvalidLength(splitCommand, 2);
+                ps.println(iStore.deleteConsumer(splitCommand[1]) ? "удалено" : "запись не найдена");
+            }
+            case "/deleteProduct" -> {
+                throwIfInvalidLength(splitCommand, 2);
+                ps.println(iStore.deleteProduct(splitCommand[1]) ? "удалено" : "запись не найдена");
+            }
+            case "/buy" -> {
+                throwIfInvalidLength(splitCommand, 3);
+                Long consumerId = getLongFromStringOrThrow(splitCommand[1], () -> new IllegalArgumentException("ИД потребителя должно быть число типа long!"));
+                Long productId = getLongFromStringOrThrow(splitCommand[2], () -> new IllegalArgumentException("ИД продукта должно быть число типа long!"));
+                var boughtProduct = iStore.buy(consumerId, productId);
+                Product product = boughtProduct.getProduct();
+                ps.printf("Пользователь %s купил продукт %s по цене %d\n", boughtProduct.getConsumer().getName(), product.getTitle(), product.getPrice());
+            }
+        }
+    }
+
+    private void throwIfInvalidLength(String[] arr, int requiredLength) {
+        if (arr.length != requiredLength)
+            throw new IllegalArgumentException("Неверное количество частей команды, должно быть " + requiredLength);
+    }
+
+    private <T extends Throwable> Long getLongFromStringOrThrow(String l, Supplier<T> throwableSupplier) throws T {
+        try {
+            return Long.parseLong(l);
+        } catch (NumberFormatException e) {
+            throw throwableSupplier.get();
+        }
+    }
+}
