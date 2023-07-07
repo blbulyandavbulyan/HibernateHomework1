be32f4a2e0357cbe24ece351fdb5845104f3992e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-07 13:52:53 +0400

    Now test uses getProductRepository and getConsumerRepository

--- src/test/java/org/blbulyandavbulyan/simplestore/services/IStoreTest.java
+++ src/test/java/org/blbulyandavbulyan/simplestore/services/IStoreTest.java
@@ -5,13 +5,14 @@ import org.blbulyandavbulyan.simplestore.entites.Consumer;
 import org.blbulyandavbulyan.simplestore.entites.Product;
 import org.blbulyandavbulyan.simplestore.services.exceptions.ConsumerNotFoundException;
 import org.blbulyandavbulyan.simplestore.services.exceptions.ProductNotFoundException;
+import org.blbulyandavbulyan.simplestore.services.interfaces.IStore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
 
 public abstract class IStoreTest {
-    protected Store store;
+    protected IStore store;
     @Test
     void getBoughtProductsByConsumerNameShouldThrowExceptionIfConsumerNotExists(){
         Assertions.assertThrows(ConsumerNotFoundException.class, ()->store.getBoughtProductsByConsumerName("Ahalaymahalay"));
@@ -26,16 +27,14 @@ public abstract class IStoreTest {
         Consumer consumer = new Consumer("John");
         Product product1 = new Product("Apple", 299L);
         Product product2 = new Product("Banana", 199L);
-        // Добавление купленных продуктов
-        store.addProduct(product1);
-        store.addProduct(product2);
-        store.addConsumer(consumer);
+        var productRepository = store.getProductRepository();
+        productRepository.addProduct(product1);
+        productRepository.addProduct(product2);
+        store.getConsumerRepository().addConsumer(consumer);
         BoughtProduct boughtProduct1 = store.buy(consumer.getId(), product1.getId());
         BoughtProduct boughtProduct2 = store.buy(consumer.getId(), product2.getId());
-
         // Выполнение метода, который тестируем
         Collection<BoughtProduct> boughtProducts = store.getBoughtProductsByConsumerName("John");
-
         // Проверка результатов
         Assertions.assertEquals(2, boughtProducts.size());
         Assertions.assertTrue(boughtProducts.contains(boughtProduct1));
@@ -46,12 +45,16 @@ public abstract class IStoreTest {
     void testGetConsumersByProductTitle() {
         // Создание тестовых данных
         Product product = new Product("Apple", 299L);
+        Product product1 = new Product("Banana", 2L);
         Consumer consumer1 = new Consumer("John");
         Consumer consumer2 = new Consumer("Alice");
         // Добавление купленных продуктов
-        store.addProduct(product);
-        store.addConsumer(consumer1);
-        store.addConsumer(consumer2);
+        var productRepository = store.getProductRepository();
+        var consumerRepository = store.getConsumerRepository();
+        productRepository.addProduct(product1);
+        productRepository.addProduct(product);
+        consumerRepository.addConsumer(consumer1);
+        consumerRepository.addConsumer(consumer2);
         store.buy(consumer1.getId(), product.getId());
         store.buy(consumer2.getId(), product.getId());
 
