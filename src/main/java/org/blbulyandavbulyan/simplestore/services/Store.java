dfc53958c57ab538964476d93b9c286381181cf7
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-07 12:06:48 +0400

    Added fields iProductsRepository and iConsumerRepository, also replaced checking existing on methods in these interfaces

--- src/main/java/org/blbulyandavbulyan/simplestore/services/Store.java
+++ src/main/java/org/blbulyandavbulyan/simplestore/services/Store.java
@@ -1,7 +1,6 @@
 package org.blbulyandavbulyan.simplestore.services;
 
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Query;
 import org.blbulyandavbulyan.simplestore.entites.BoughtProduct;
 import org.blbulyandavbulyan.simplestore.entites.Consumer;
 import org.blbulyandavbulyan.simplestore.entites.Product;
@@ -21,11 +20,13 @@ import static org.blbulyandavbulyan.simplestore.utils.ORMUtils.runInTransaction;
  * Предоставляет базовую имплементацию интерфейса IStore.
  * Использует EntityManagerFactory для сохранения сущностей в базу
  */
-public class Store implements IStore, IConsumersRepository, IProductsRepository {
+public class Store implements IStore {
     /**
      * Фабрика, с помощью которой будут создаваться EntityManger для управления сущностями в базе
      */
     private final EntityManagerFactory emf;
+    private final IProductsRepository iProductsRepository;
+    private final IConsumersRepository iConsumersRepository;
 
     /**
      * Создаёт экземпляр магазина
@@ -37,15 +38,15 @@ public class Store implements IStore, IConsumersRepository, IProductsRepository 
         if (emf == null || !emf.isOpen())
             throw new IllegalArgumentException("Entity manager factory is null or closed!");
         this.emf = emf;
+        iProductsRepository = new ProductsRepository(emf);
+        iConsumersRepository = new ConsumersRepository(emf);
     }
 
     @Override
     public Collection<BoughtProduct> getBoughtProductsByConsumerName(String name) {
         if (name == null) throw new IllegalArgumentException("name is null!");
         return runForEntityManager(emf, em -> {
-            var checkExistConsumerQuery = em.createQuery("SELECT COUNT(c) FROM Consumer c WHERE c.name = :name", Long.class);
-            checkExistConsumerQuery.setParameter("name", name);
-            if (checkExistConsumerQuery.getSingleResult() > 0) {
+            if (iConsumersRepository.existsByName(name)) {
                 var selectBoughtProductsQuery = em.createQuery("SELECT c.boughtProducts FROM Consumer c WHERE c.name = :name", BoughtProduct.class);
                 selectBoughtProductsQuery.setParameter("name", name);
                 return selectBoughtProductsQuery.getResultList();
@@ -57,9 +58,7 @@ public class Store implements IStore, IConsumersRepository, IProductsRepository 
     public Collection<Consumer> getConsumersByProductTitle(String title) {
         if (title == null) throw new IllegalArgumentException("title is null!");
         return runForEntityManager(emf, em -> {
-            var checkExistConsumerQuery = em.createQuery("SELECT COUNT(p) FROM Product p WHERE p.title = :title", Long.class);
-            checkExistConsumerQuery.setParameter("title", title);
-            if (checkExistConsumerQuery.getSingleResult() > 0) {
+            if (iProductsRepository.existsByTitle(title)) {
                 var selectConsumersByProductTitleQuery = em.createQuery("SELECT bp.consumer FROM BoughtProduct bp WHERE bp.product.title = :title", Consumer.class);
                 selectConsumersByProductTitleQuery.setParameter("title", title);
                 return selectConsumersByProductTitleQuery.getResultList();
@@ -67,25 +66,6 @@ public class Store implements IStore, IConsumersRepository, IProductsRepository 
         });
     }
 
-    @Override
-    public boolean deleteConsumer(String name) {
-        if (name == null) throw new IllegalArgumentException("name is null!");
-        return runInTransaction(emf, (em) -> {
-            Query deleteConsumerQuery = em.createQuery("DELETE FROM Consumer c WHERE c.name = :name");
-            int result = deleteConsumerQuery.setParameter("name", name).executeUpdate();
-            return result > 0;//если result > 0 значит были затронуты записи запросом
-        });
-    }
-
-    @Override
-    public boolean deleteProduct(String title) {
-        if (title == null) throw new IllegalArgumentException("title is null!");
-        return runInTransaction(emf, (em) -> {
-            Query deleteProductQuery = em.createQuery("DELETE FROM Product p WHERE p.title = :title");
-            int result = deleteProductQuery.setParameter("title", title).executeUpdate();
-            return result > 0;//если result > 0 значит были затронуты записи запросом
-        });
-    }
 
     @Override
     public BoughtProduct buy(Long consumerId, Long productId) {
@@ -100,26 +80,6 @@ public class Store implements IStore, IConsumersRepository, IProductsRepository 
             BoughtProduct boughtProduct = new BoughtProduct(product, consumer, product.getPrice());//создаём запись о купленном продукте
             em.persist(boughtProduct);//сохраняем запись о покупке в базу
             return boughtProduct;
-        });
-    }
-
-    @Override
-    public void addProduct(Product product) {
-        if (product == null) throw new IllegalArgumentException("product is null!");
-        runInTransaction(emf, em -> {
-            em.persist(product);
-            //Поскольку у нас тип у лямды Function, мы обязательно должны что-то вернуть,
-            // возвращаем null, т.к. больше нечего вернуть
-            return null;
-        });
-    }
-
-    @Override
-    public void addConsumer(Consumer consumer) {
-        if (consumer == null) throw new IllegalArgumentException("consumer is null!");
-        runInTransaction(emf, em -> {
-            em.persist(consumer);
-            return null;
         });
     }
 }
