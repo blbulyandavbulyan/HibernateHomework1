e7b87877c805882130788ce3c1ea3ccaf345abd1
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-09 22:30:58 +0400

    Javadoc descriptions for FileNotFoundInResources added

--- src/main/java/org/blbulyandavbulyan/simplestore/utils/exceptions/FileNotFoundInResources.java
+++ src/main/java/org/blbulyandavbulyan/simplestore/utils/exceptions/FileNotFoundInResources.java
@@ -1,5 +1,8 @@
 package org.blbulyandavbulyan.simplestore.utils.exceptions;
 
+/**
+ * Данное исключение бросается если не был найден файл в ресурсах
+ */
 public class FileNotFoundInResources extends RuntimeException{
     public FileNotFoundInResources(String message) {
         super(message);
