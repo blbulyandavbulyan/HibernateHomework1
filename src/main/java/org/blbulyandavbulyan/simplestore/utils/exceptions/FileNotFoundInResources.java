6d2908ce9d20b9833da15afd43f356fe4f3374ef
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-09 21:20:30 +0400

    Added FileNotFoundInResources in exceptions for ORMUtils

---
+++ src/main/java/org/blbulyandavbulyan/simplestore/utils/exceptions/FileNotFoundInResources.java
@@ -0,0 +1,7 @@
+package org.blbulyandavbulyan.simplestore.utils.exceptions;
+
+public class FileNotFoundInResources extends RuntimeException{
+    public FileNotFoundInResources(String message) {
+        super(message);
+    }
+}
